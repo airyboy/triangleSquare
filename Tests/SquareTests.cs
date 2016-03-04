7f316d173bf10ef67122f0fc9e012ddd43af60ea
Airy <clevermik@ya.ru>
2016-03-04 03:15:28 +0300

    initial commit

---
+++ Tests/SquareTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using TriangleCalc;
+
+namespace Tests
+{
+    public class TriangleSquareTests
+    {
+        [Fact]
+        public void the_square_is_correct()
+        {
+            Assert.True(TriangleSquare.CalcSquare(3,4,5) == 6);
+        }
+
+        [Fact]
+        public void on_negative_sides_throws_exception()
+        {
+            Assert.Throws(typeof(TriangleSquare.IncorrectSideLengthException), () => TriangleSquare.CalcSquare(-1, 0, 1));
+        }
+
+        [Fact]
+        public void on_impossible_triangle_throws_exception()
+        {
+            Assert.Throws(typeof(TriangleSquare.ImpossibleTriangleException), () => TriangleSquare.CalcSquare(10, 4, 4));
+        }
+    }
+}
---
+++ TriangleSquare/TriangleSquare.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriangleCalc
+{
+    public class TriangleSquare
+    {
+        public class IncorrectSideLengthException : Exception
+        {
+            public IncorrectSideLengthException() { }
+        }
+
+        public class ImpossibleTriangleException : Exception
+        {
+            public ImpossibleTriangleException() { }
+        }
+
+        public static double CalcSquare(double a, double b, double c)
+        {
+            //checking for non-zero and negative values
+            if (a <= 0 | b <= 0 | c <= 0)
+            {
+                throw new IncorrectSideLengthException();
+            }
+
+            //checking triangle inequality as well
+            if (a + b < c | a + c < b | b + c < a)
+            {
+                throw new ImpossibleTriangleException();
+            }
+
+            // Heron's formula
+            var semiPerimeter = (a + b + c) / 2;
+            var s = Math.Sqrt(semiPerimeter * (semiPerimeter - a) * (semiPerimeter - b) * (semiPerimeter - c));
+            return s; 
+        }
+    }
+}
